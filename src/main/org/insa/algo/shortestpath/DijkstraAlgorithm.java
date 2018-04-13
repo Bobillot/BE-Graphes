ec4f7b05822268ea570c7abfd379c57effb83ba3
Mme Marotte Loica <marotte@insa-10225>
2018-04-13 17:04:59 +0200

    Création de Label et début de dijkstra

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,11 @@
 package org.insa.algo.shortestpath;
 
+import java.util.Arrays;
+
+import org.insa.graph.Graph;
+import org.insa.graph.Node;
+import org.insa.algo.utils.*;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -7,10 +13,45 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     }
 
     @Override
-    protected ShortestPathSolution doRun() {
+    protected ShortestPathSolution doRun() 
+    {
+    	// --------------------initialisation --------------------
+    	// recup le graphe
         ShortestPathData data = getInputData();
+		Graph graph = data.getGraph();
+		
+		// structure de la solution
         ShortestPathSolution solution = null;
-        // TODO:
+        
+        // variables utiles
+		final int nbNodes = graph.size();
+        
+        
+        // init du tableau de label(premiere ligne avec des infini et un zero dans le noeud de depart)
+		Label[] distances = new Label[nbNodes];
+		int i = 0 ;
+		for (Node node : graph)
+		{
+			if (node.equals(data.getOrigin()))
+			{
+				distances[i] = new Label(node,0,null,0) ;
+			}
+			else
+			{
+				distances[i]= new Label(node,Double.POSITIVE_INFINITY,null,0) ;
+			}
+			
+			i++ ;		
+		}
+		
+        // --------------------iterations --------------------
+		BinaryHeap<Label> Tas = new BinaryHeap() ;
+		
+		
+		
+        
+        
+
         return solution;
     }
 
---
+++ src/main/org/insa/algo/shortestpath/Label.java
@@ -0,0 +1,70 @@
+package org.insa.algo.shortestpath;
+import org.insa.graph.*;
+
+public class Label implements Comparable<Label>
+{
+	private Node noeud ;
+	private double cout ;
+	private Node precedent ;
+	private int marquage ;
+	
+	// constructeur
+	public Label (Node noeud, double cout, Node precedent, int marquage)
+	{
+		this.noeud = noeud ;
+		this.cout = cout ;
+		this.precedent = precedent ;
+		this.marquage = marquage ;
+	}
+	
+	// getter
+	public Node getNoeud()
+	{
+		return this.noeud ;
+	}
+	public double getCout()
+	{
+		return this.cout ;
+	}
+	public Node getPrecedent()
+	{
+		return this.precedent ;
+	}
+	public int getMarquage()
+	{
+		return this.marquage ;
+	}
+	
+	// setter
+	public void setCout(double cout)
+	{
+		this.cout = cout ;
+	}
+	public void getPrecedent(Node noeud)
+	{
+		this.precedent = noeud ;
+	}
+	public void getMarquage(int marquage)
+	{
+		this.marquage = marquage;
+	}
+	
+	// fonction de comparaison retourne (this-autre)
+	public int compareTo(Label autre)
+	{
+		if(this.getCout()-autre.getCout()<0) 
+		{
+			return -1 ;
+		}
+		else if(this.getCout()-autre.getCout()>0)
+		{
+			return 1 ;
+		}
+		else
+		{
+			return 0 ;
+		}
+	}
+	
+	
+}
